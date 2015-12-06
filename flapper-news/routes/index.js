429c5874fba1ae4c0d8c3aa2eef3b444fec0dd60
Ismael Navarro <inavarro@aibegroup.com>
2015-12-06 21:23:59 +0100

    Añadida autentificación

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var jwt = require('express-jwt');
+
+var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
 
 var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
 
 //Middleware para los post
 router.param('post', function(req, res, next, id) {
@@ -42,9 +47,9 @@ router.get("/posts", function(req, res, next){
 });
 
 //Creación de un POST
-router.post("/posts", function(req, res, next){
+router.post("/posts", auth, function(req, res, next){
 	var post = new Post(req.body);
-	console.log(req.body);
+  post.author = req.payload.username;
 	post.save(function(err, post) {
 		if(err)
 			return next(err);
@@ -62,19 +67,20 @@ router.get('/posts/:post', function(req, res) {
 });
 
 //Subir los votos de un comentario en particular
-router.put('/posts/:post/upvote', function(req, res, next) {
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
   req.post.upvote(function(err, post){
     if (err) { return next(err); }
 
     res.json(post);
   });
-})
+});
 
 //Rutas de gestión de los Comments
 //Crea un nuevo Comment en un Post
-router.post('/posts/:post/comments', function(req, res, next) {
+router.post('/posts/:post/comments', auth, function(req, res, next) {
   var comment = new Comment(req.body);
   comment.post = req.post;
+  comment.author = req.payload.username;
 
   comment.save(function(err, comment){
     if(err){ return next(err); }
@@ -89,12 +95,48 @@ router.post('/posts/:post/comments', function(req, res, next) {
 });
 
 //Sube los votos de los comentarios de un post
-router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
 	req.comment.upvote(function(err, comment) {
 		if(err) { return next(err); }
 
 		res.json(comment);
 	});
+});
+
+//Rutas para la gestión de usuarios
+router.post('/register', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  var user = new User();
+
+  user.username = req.body.username;
+
+  user.setPassword(req.body.password);
+
+  user.save(function (err){
+    if(err){ return next(err); }
+
+    return res.json({token: user.generateJWT()});
+  });
+});
+
+
+router.post('/login', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  passport.authenticate('local', function(err, user, info){
+    if(err){ return next(err); }
+
+    if(user){
+      return res.json({token: user.generateJWT()});
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
 });
 
 /* GET home page. */
